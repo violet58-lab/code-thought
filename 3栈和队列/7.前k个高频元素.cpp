95277eb28803828a0db1ea53898d6e36e89e8444
violet58-lab <chenviolet58@gmail.com>
2026-04-05 15:13:36 +0800

    有难度

--- 3栈和队列/7.前k个高频元素.cpp
+++ 3栈和队列/7.前k个高频元素.cpp
@@ -5,20 +5,20 @@ public:
     // 小顶堆
     class mycomparison {
     public:
-        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {//比较器
             return lhs.second > rhs.second;
         }
     };
     vector<int> topKFrequent(vector<int>& nums, int k) {
         // 要统计元素出现频率
-        unordered_map<int, int> map; // map<nums[i],对应出现的次数>
+        unordered_map<int, int> map; // map<nums[i],对应出现的次数>。表示键值的哈希表
         for (int i = 0; i < nums.size(); i++) {
             map[nums[i]]++;
         }
 
         // 对频率排序
         // 定义一个小顶堆，大小为k
-        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;//自定义比较规则的优先级队列
 
         // 用固定大小为k的小顶堆，扫面所有频率的数值
         for (unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++) {
@@ -31,7 +31,7 @@ public:
         // 找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
         vector<int> result(k);
         for (int i = k - 1; i >= 0; i--) {
-            result[i] = pri_que.top().first;
+            result[i] = pri_que.top().first;//访问优先级队列堆顶元素里的first，先弹出来的是频率最大的
             pri_que.pop();
         }
         return result;
